0d1fee3d103fd9ac956688e3bbc723efa8d7a7e5
Dhruv00421 <bamaniyadhruv3@gmail.com>
2023-07-14 10:09:33 +0530

    change

--- script/main.js
+++ script/main.js
@@ -1,1 +1,47 @@
-var
+// var IMG = document.getElementById("photo");
+// IMG.addEventListener("click", picimg);
+
+// function picimg() {
+//     var Images = document.querySelector("img","video");
+
+//     for(var i=0; i < Images.length; i++){
+//         Images[i].className = "hide";
+//     }
+
+//     var picId = this.attributes["data-img"].value;
+//     var pic = document.getElementById(picId);
+
+//     if(pic.className === "hide") {
+//         pic.className = "";
+//     }
+//     else {
+//         pic.className = "hide";
+//     }
+// }
+// var video = document.getElementsByClassName("background-clip");
+// var image = document.getElementsByClassName("pic");
+// var IMG = document.getElementById("photo");
+
+// IMG.addEventListener("click", change);
+
+// function change() {
+//     if(pic.Id === "hide" || background-Clip.Id === "nonhide") {
+//         pic.Id === "";
+//         background-clip.Id  === "hide";
+//     }
+//     else {
+//         pic.Id === "hide";
+//         background-clip.Id === "nonhide";
+//     }
+// }
+
+// var sound = document.getElementsByClassName("sound");
+
+// sound.addEventListener("click", playsound);
+
+// function playsound(audioName,loop) {
+//     let audio = new Audio(audioName);
+//     audio.loop = loop;
+//     audio.play();
+// }
+// playsound("coc intro",true);
